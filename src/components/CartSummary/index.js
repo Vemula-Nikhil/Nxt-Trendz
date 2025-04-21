23f4d0f473926a909f3b8732d6529f0f9a3f3c8e
Vemula-Nikhil <vemulanikhil0810@gmail.com>
2025-04-21 19:40:38 +0530

    Update index.js

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,40 +1,195 @@
-// Write your code here
+import Popup from 'reactjs-popup'
+
+import {IoClose, IoCashOutline} from 'react-icons/io5'
+import {FaIdCard} from 'react-icons/fa'
+import {AiFillBank, AiOutlineWallet} from 'react-icons/ai'
+import {RiAmazonLine} from 'react-icons/ri'
+
+import {useState} from 'react'
+
 import CartContext from '../../context/CartContext'
 
 import './index.css'
 
-const CartSummary = () => (
-  <CartContext.Consumer>
-    {value => {
-      const {cartList} = value
+const CartSummary = () => {
+  const [cardPayment, setCardPayment] = useState(false)
+  const [netBanking, setNetBanking] = useState(false)
+  const [upi, setUpiPayment] = useState(false)
+  const [wallet, setWalletPayment] = useState(false)
+  const [confirmOrder, setConfirmOrder] = useState(false)
 
-      const getTotalAmount = () => {
-        const totalAmountList = cartList.map(each => each.quantity * each.price)
-        console.log(totalAmountList)
-        const reducer = (accumulator, currentValue) =>
-          accumulator + currentValue
+  const onClickCardButton = () => {
+    setCardPayment(!cardPayment)
+    setNetBanking(false)
+    setUpiPayment(false)
+    setWalletPayment(false)
+  }
+  const onClickNetBankingButton = () => {
+    setNetBanking(!netBanking)
+    setCardPayment(false)
+    setUpiPayment(false)
+    setWalletPayment(false)
+  }
+  const onClickUpiButton = () => {
+    setUpiPayment(!upi)
+    setCardPayment(false)
+    setNetBanking(false)
+    setWalletPayment(false)
+  }
+  const onClickWalletButton = () => {
+    setWalletPayment(!wallet)
+    setCardPayment(false)
+    setNetBanking(false)
+    setUpiPayment(false)
+  }
 
-        return totalAmountList.reduce(reducer)
-      }
+  const onClickCODButton = () => {
+    setWalletPayment(false)
+    setCardPayment(false)
+    setNetBanking(false)
+    setUpiPayment(false)
+  }
 
-      getTotalAmount()
+  const isPaymentSelected = cardPayment || netBanking || upi || wallet
+  const confirmOrderClassName = isPaymentSelected ? 'confirm-order' : ''
+  const cardPaymentClassName = cardPayment ? 'confirm-order' : ''
+  const netBankingClassName = netBanking ? 'confirm-order' : ''
+  const upiClassName = upi ? 'confirm-order' : ''
+  const walletClassName = wallet ? 'confirm-order' : ''
 
-      return (
-        <div className="order-summary-container">
-          <h1 className="order-total">
-            Order Total:{' '}
-            <span className="order-total-price">Rs {getTotalAmount()}</span>
-          </h1>
-          <p className="order-summary-quantity">
-            {cartList.length} Items in cart
-          </p>
-          <button type="button" className="checkout-button">
-            Checkout
-          </button>
-        </div>
-      )
-    }}
-  </CartContext.Consumer>
-)
+  const onClickConfirmOrder = () => {
+    if (isPaymentSelected) {
+      setConfirmOrder(true)
+    }
+  }
+
+  return (
+    <CartContext.Consumer>
+      {value => {
+        const {cartList} = value
+
+        const getTotalAmount = () => {
+          const totalAmountList = cartList.map(
+            each => each.quantity * each.price,
+          )
+          console.log(totalAmountList)
+          const reducer = (accumulator, currentValue) =>
+            accumulator + currentValue
+
+          return totalAmountList.reduce(reducer)
+        }
+
+        getTotalAmount()
+
+        return (
+          <div className="order-summary-container">
+            <h1 className="order-total">
+              Order Total:{' '}
+              <span className="order-total-price">Rs {getTotalAmount()}</span>
+            </h1>
+            <p className="order-summary-quantity">
+              {cartList.length} Items in cart
+            </p>
+            <div className="popup-card">
+              <Popup
+                modal
+                trigger={
+                  <button type="button" className="checkout-button">
+                    Checkout
+                  </button>
+                }
+              >
+                {close => (
+                  <>
+                    <div className="popup-container">
+                      <div className="payments-details">
+                        <div className="payments-container">
+                          <h1 className="payments-heading">Payments</h1>
+                          <button
+                            type="button"
+                            className={`payment-button ${cardPaymentClassName}`}
+                            onClick={onClickCardButton}
+                          >
+                            <FaIdCard className="payment-icon" />
+                            <p className="payment-type">
+                              Credit / Dedit / ATM Card
+                            </p>
+                          </button>
+                          <button
+                            type="button"
+                            className={`payment-button ${netBankingClassName}`}
+                            onClick={onClickNetBankingButton}
+                          >
+                            <AiFillBank className="payment-icon" />
+                            <p className="payment-type">Net Banking</p>
+                          </button>
+                          <button
+                            type="button"
+                            className={`payment-button ${upiClassName}`}
+                            onClick={onClickUpiButton}
+                          >
+                            <RiAmazonLine className="payment-icon" />
+                            <p className="payment-type">UPI</p>
+                          </button>
+                          <button
+                            type="button"
+                            className={`payment-button ${walletClassName}`}
+                            onClick={onClickWalletButton}
+                          >
+                            <AiOutlineWallet className="payment-icon" />
+                            <p className="payment-type">Wallet</p>
+                          </button>
+                          <button
+                            type="button"
+                            className="payment-button cod"
+                            onClick={onClickCODButton}
+                          >
+                            <IoCashOutline className="payment-icon" />
+                            <p className="payment-type">
+                              Cash on Delivery{' '}
+                              <span className="unavailable">Unavailable</span>
+                            </p>
+                          </button>
+                        </div>
+                        <div className="summary-container">
+                          <h1 className="payments-heading">Summary</h1>
+                          <div className="price-details-container">
+                            <p className="total-items">
+                              Total Price ({cartList.length})
+                            </p>
+                            <p className="price">₹{getTotalAmount()}</p>
+                          </div>
+                          <button
+                            type="button"
+                            className={`confirm-btn ${confirmOrderClassName}`}
+                            onClick={onClickConfirmOrder}
+                          >
+                            Confirm Order
+                          </button>
+                          {confirmOrder && (
+                            <p className="confirm-order-text">
+                              Your order has been placed successfully
+                            </p>
+                          )}
+                        </div>
+                      </div>
+                      <button
+                        type="button"
+                        className="trigger-button"
+                        onClick={() => close()}
+                      >
+                        <IoClose className="close-icon" />
+                      </button>
+                    </div>
+                  </>
+                )}
+              </Popup>
+            </div>
+          </div>
+        )
+      }}
+    </CartContext.Consumer>
+  )
+}
 
 export default CartSummary
